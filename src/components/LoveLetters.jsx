c2c9a48d398d453d19b5ba75190f79ceb99883e0
AamHermansyah <amzhermanzyah@gmail.com>
2023-04-21 09:33:20 +0700

    hidden button

--- src/components/LoveLetters.jsx
+++ src/components/LoveLetters.jsx
@@ -5,7 +5,7 @@ import { motion as m } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 function LoveLetters() {
-  const [isDisabled, setIsDisabled] = useState(true);
+  const [displayButton, setDisplayButton] = useState(true);
   const [isReject, setIsReject] = useState(null);
   const [isAccept, setIsAccept] = useState(false);
   const [inputErrorMessage, setInputErrorMessage] = useState(null);
@@ -31,7 +31,7 @@ function LoveLetters() {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setIsDisabled(false);
+      setDisplayButton(false);
     }, 57000);
 
     return () => {
@@ -97,35 +97,36 @@ function LoveLetters() {
       )}
       <div className="max-w-[700px] mx-auto p-4">
         <SmoothTypingText stagger={.03} text={LOVE_LETTERS} textStyles="text-md mt-4" />
-        <div className="mt-6">
-          <button
-            disabled={isDisabled}
-            onClick={() => {
-              if (isAccept) {
-                setStepIndex((prev) => prev + 1);
-                return;
-              }
-              setIsAccept(true);
-            }}
-            type="button"
-            className="block w-full mx-auto px-4 py-2 rounded-full bg-sky-500 hover:bg-sky-600 transition-colors duration-200 text-white disabled:cursor-not-allowed disabled:opacity-50"
-          >
-            {isAccept ? 'Benerrr nih??😍💕' : 'Terima:)❤️🥺'}
-          </button>
-          <button
-            onClick={() => {
-              if (isReject === null) setIsReject(false);
-              if (isReject === false) setIsReject(true);
-            }}
-            disabled={isDisabled || isReject}
-            type="button"
-            className="mb-4 mt-2 block w-full mx-auto px-4 py-2 rounded-full bg-red-500 hover:bg-red-600 transition-colors duration-200 text-white disabled:cursor-not-allowed disabled:opacity-50"
-          >
-            {isReject === null && 'Tolak😊'}
-            {isReject === false && 'Serius ga mau?🥺'}
-            {isReject === true && 'Harus diterima pokonya😫'}
-          </button>
-        </div>
+        {displayButton && (
+          <div className="mt-6">
+            <button
+              onClick={() => {
+                if (isAccept) {
+                  setStepIndex((prev) => prev + 1);
+                  return;
+                }
+                setIsAccept(true);
+              }}
+              type="button"
+              className="block w-full mx-auto px-4 py-2 rounded-full bg-sky-500 hover:bg-sky-600 transition-colors duration-200 text-white disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              {isAccept ? 'Benerrr nih??😍💕' : 'Terima:)❤️🥺'}
+            </button>
+            <button
+              onClick={() => {
+                if (isReject === null) setIsReject(false);
+                if (isReject === false) setIsReject(true);
+              }}
+              disabled={isReject}
+              type="button"
+              className="mb-4 mt-2 block w-full mx-auto px-4 py-2 rounded-full bg-red-500 hover:bg-red-600 transition-colors duration-200 text-white disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              {isReject === null && 'Tolak😊'}
+              {isReject === false && 'Serius ga mau?🥺'}
+              {isReject === true && 'Harus diterima pokonya😫'}
+            </button>
+          </div>
+        )}
       </div>
     </section>
   )
